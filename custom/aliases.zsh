ecb4b4c0678fb2b4d7ddcb3b46831221ec965e1d
Ian Truslove <ian.truslove@gmail.com>
2013-10-02 17:28:29 -0600

    Some minor tweaks

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,4 +1,4 @@
 [[ -x /usr/local/bin/vim ]] && alias vim=/usr/local/bin/vim && export EDITOR=/usr/local/bin/vim
-[[ -x /usr/local/bin/emacs-24.2 ]] && alias emacs='/usr/local/bin/emacs-24.2 -nw'
+#[[ -x /usr/local/bin/emacs-24.2 ]] && alias emacs='/usr/local/bin/emacs-24.2 -nw'
 alias ll='ls -l'
 alias l='ll -a'
